b74feccde8b503e5e0079897e35afc868c17e324
Markus Hofmann <m.hofmann@aa-sec.com>
2024-01-25 18:25:17 +0100

    fix: fixed buffer overflow if para is creater then 77 chars

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 //Alternativ diesen Ordner mit VSCode oeffnen
 #include <config.h>
 #include <cstring>
-#
 #include <iostream>
 using namespace std; // = std::
 
@@ -24,7 +23,7 @@ int main(int argc, const char** argv) { //argv[][]
     }
     // Call Fuctions 
     Bing_command_block(argv);
-    Self_command_block(argv);
+    //Self_command_block(argv);
 
 
 
@@ -61,11 +60,42 @@ void Bing_command_block(const char** argv)
     border += "+";
 
     cout << border << endl;
-    cout << "| " << text;
-    for (int i = 0; i < 77 - text.length(); i++) {
-        cout << " ";
+    //cout << "| " << text;
+    //string* b;
+    if(text.length() > 77)
+    {
+        //cout << "if Treu" << endl;
+        int nullst = 0;
+        for (int a = 0; a < (text.length() / 77) ; a++)
+        {
+
+            cout << "| " << text.substr(nullst,nullst += 77) << "|" << endl;
+            cout << nullst << endl;
+
+        }
+        cout << "| " << text.substr(nullst,nullst += (text.length() % 77));
+        for (int i = 0; i < 77 - (text.length() % 77); i++) {
+            cout << " ";
+        }
+        cout << "|" << endl;
     }
-    cout << "|" << endl;
+    else
+    {
+        cout << "| " << text;
+        for (int i = 0; i < 77 - text.length(); i++) {
+            cout << " ";
+        }
+        cout << "|" << endl;
+    }
     cout << border << endl;
+
+}
+void Self_command_block(char** argv)
+{
+    /*
+    Das ist die selbst geschrieben Command Block Function
+    Diese verwendet für die Umsetzung nicht die String Klasse 
+    */
+
 
 }
