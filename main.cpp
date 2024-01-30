0066be1a69348dfc95559a3c40b11ef71900b734
Markus Hofmann <m.hofmann@aa-sec.com>
2024-01-30 10:54:49 +0100

    feat: added Vorlesungslösung of Command Block

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-//Erstellen des Programm
-//mkdir build
-//cd build
-//cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ..
-//ninja
-//Alternativ diesen Ordner mit VSCode oeffnen
+// Erstellen des Programm
+// mkdir build
+// cd build
+// cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ..
+// ninja
+// Alternativ diesen Ordner mit VSCode oeffnen
 #include <config.h>
 #include <cstring>
 #include <iostream>
@@ -20,30 +20,33 @@
 */
 using namespace std; // = std::
 
-void StringCommandBlock(const char** argv);
-void NoStringCommandBlock(const char** argv);
-void NoStringOverflowCommandBlock(const char** argv);
-void VorlesungsCommandBlock(const char** argv);
+void StringCommandBlock(const char **argv);
+void NoStringCommandBlock(const char **argv);
+void NoStringOverflowCommandBlock(const char **argv);
+void VorlesungsCommandBlock(const char **argv);
 
 // Vorlesung Tag 4
-enum {
+enum
+{
     SIGN_PER_LINE = 01,
     START_ADDRESS_LINE01 = 0,
     START_ADDRESS_LINE02 = SIGN_PER_LINE,
     START_OF_TEXT = 0,
     MAX_TEXT_LENGTH = SIGN_PER_LINE - 2 - 2 - 1,
-    //OVERFLOWPRINTPOSITION = 0,
+    // OVERFLOWPRINTPOSITION = 0,
     START_ADDRESS_LINE03 = SIGN_PER_LINE * 2,
     TOTAL_BUFFER_SIZE = SIGN_PER_LINE * 3
 };
 
-int main(int argc, const char** argv) { //argv[][]
-    //std::cout << argv[0] << endl;
-    //std::cout << argv[1] << endl;
-    
-    if (argc != 2) {
+int main(int argc, const char **argv)
+{ // argv[][]
+    // std::cout << argv[0] << endl;
+    // std::cout << argv[1] << endl;
+
+    if (argc != 2)
+    {
         cerr << "Bitte nur ein Argument mitgeben" << endl;
-        //return 1;
+        // return 1;
         return EXIT_FAILURE;
     }
     /*
@@ -62,21 +65,20 @@ int main(int argc, const char** argv) { //argv[][]
          << "Version:      " << MAIN_VERSION_MAJOR << '.' << MAIN_VERSION_MINOR << '\n'
          << "Beschreibung: " << MAIN_DESCRIPTION << endl;
 
-    */    
-
+    */
 
     return EXIT_SUCCESS;
 }
 
-void StringCommandBlock(const char** argv)
+void StringCommandBlock(const char **argv)
 {
     /**
-    * Die Function nutzt statt char pointer array die string klasse
-    * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines. 
-    * Die std:string Klasse kapselt Zeichenarrays und biete nützliche Methoden zb append().
-    * std:string kümmert sich selbst um Speicherverwaltung.
-    * @param argv Parameter der von der Kommandozeile übergeben wird. 
-    **/
+     * Die Function nutzt statt char pointer array die string klasse
+     * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines.
+     * Die std:string Klasse kapselt Zeichenarrays und biete nützliche Methoden zb append().
+     * std:string kümmert sich selbst um Speicherverwaltung.
+     * @param argv Parameter der von der Kommandozeile übergeben wird.
+     **/
     cout << "StringCommandBlock" << endl;
     string text = argv[1];
     // Print text lengh
@@ -84,27 +86,28 @@ void StringCommandBlock(const char** argv)
     cout << text.length() << endl;
 
     string border = "+";
-    for (int i = 0; i < 78; i++) {
+    for (int i = 0; i < 78; i++)
+    {
         border += "-";
     }
     border += "+";
 
     cout << border << endl;
 
-    if(text.length() > 77)
+    if (text.length() > 77)
     {
         int nullst = 0;
-        int cut = 77; 
-        for (int i = 0; i < (text.length() / 77) ; i++)
+        int cut = 77;
+        for (int i = 0; i < (text.length() / 77); i++)
         {
 
-            //cout << "| " << text.substr(nullst, cut).length() << "|" << endl;
+            // cout << "| " << text.substr(nullst, cut).length() << "|" << endl;
             cout << "| " << text.substr(nullst, cut) << "|" << endl;
             nullst = nullst + cut;
-
         }
         cout << "| " << text.substr(nullst, nullst + (text.length() % 77));
-        for (int i = 0; i < 77 - (text.length() % 77); i++) {
+        for (int i = 0; i < 77 - (text.length() % 77); i++)
+        {
             cout << " ";
         }
         cout << "|" << endl;
@@ -112,27 +115,27 @@ void StringCommandBlock(const char** argv)
     else
     {
         cout << "| " << text;
-        for (int i = 0; i < 77 - text.length(); i++) {
+        for (int i = 0; i < 77 - text.length(); i++)
+        {
             cout << " ";
         }
         cout << "|" << endl;
     }
     cout << border << endl;
-
 }
-void NoStringCommandBlock(const char** argv)
+void NoStringCommandBlock(const char **argv)
 {
     /**
      * Diese Funktion verwendet für die Umsetzung des Command Blocks nicht die String Klasse.
-     * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines. 
-     * @param argv Parameter der von der Kommandozeile übergeben wird. 
-    **/
+     * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines.
+     * @param argv Parameter der von der Kommandozeile übergeben wird.
+     **/
 
     // Speicher resevieren
-    char* border = new char[80];
-    char* text = new char[80];
-    const char* bp = new char[2];
-    const char* bm = new char[2];
+    char *border = new char[80];
+    char *text = new char[80];
+    const char *bp = new char[2];
+    const char *bm = new char[2];
     memset(border, 0, 80);
     memset(text, 0, 80);
     bp = "+";
@@ -140,7 +143,8 @@ void NoStringCommandBlock(const char** argv)
     cout << "NoStringCommandBlock" << endl;
 
     strcat(border, bp);
-    for (int i = 0; i < 78; i++) {
+    for (int i = 0; i < 78; i++)
+    {
         strcat(border, bm);
     }
     strcat(border, bp);
@@ -157,13 +161,13 @@ void NoStringCommandBlock(const char** argv)
                 cout << text[i];
             }
             strstart = strstart + 77;
-            cout << "|"  << endl;
+            cout << "|" << endl;
         }
         if ((strlen(argv[1]) % 77) != 0)
         {
             cout << "| ";
             /*
-            test der berechnung der Stringlänge 
+            test der berechnung der Stringlänge
             cout << "For:" << endl;
             cout << strstart << endl;
             cout <<  strstart + (strlen(argv[1]) % 77) << endl;
@@ -179,9 +183,6 @@ void NoStringCommandBlock(const char** argv)
             }
             cout << "|" << endl;
         }
-
-        
-        
     }
     else
     {
@@ -190,7 +191,7 @@ void NoStringCommandBlock(const char** argv)
         {
             strcat(text, " ");
         }
-        
+
         cout << "| " << text << "|" << endl;
     }
 
@@ -198,18 +199,18 @@ void NoStringCommandBlock(const char** argv)
     delete[] border;
     delete[] text;
 }
-void NoStringOverflowCommandBlock(const char** argv)
+void NoStringOverflowCommandBlock(const char **argv)
 {
     /**
      * Diese Funktion verwendet für die Umsetzung des Command Blocks nicht die String Klasse.
-     * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines. 
-     * @param argv Parameter der von der Kommandozeile übergeben wird. 
-    **/
+     * Die Funktion splitted den String nach 80 Zeichen und und erweiterte die Box mit newlines.
+     * @param argv Parameter der von der Kommandozeile übergeben wird.
+     **/
 
-    char* border = new char[80];
-    char* text = new char[80];
-    const char* bp = new char[2];
-    const char* bm = new char[2];
+    char *border = new char[80];
+    char *text = new char[80];
+    const char *bp = new char[2];
+    const char *bm = new char[2];
     memset(border, 0, 80);
     memset(text, 0, 80);
     bp = "+";
@@ -217,7 +218,8 @@ void NoStringOverflowCommandBlock(const char** argv)
     strcat(border, bp);
     cout << "NoStringOverflowCommandBlock" << endl;
 
-    for (int i = 0; i < 78; i++) {
+    for (int i = 0; i < 78; i++)
+    {
         strcat(border, bm);
     }
     strcat(border, bp);
@@ -228,7 +230,6 @@ void NoStringOverflowCommandBlock(const char** argv)
         {
             /* code */
         }
-        
     }
     else
     {
@@ -237,53 +238,50 @@ void NoStringOverflowCommandBlock(const char** argv)
         {
             strcat(text, " ");
         }
-        
+
         cout << "| " << text << "|" << endl;
     }
-
-    
-
 }
-void VorlesungsCommandBlock(const char** argv)
+void VorlesungsCommandBlock(const char **argv)
 {
-    char* buffer = new char[TOTAL_BUFFER_SIZE];
+    char *buffer = new char[TOTAL_BUFFER_SIZE];
 
-        if (!buffer) {
-            cerr << "Keins Speicher zu verfügung" << endl;
+    if (!buffer)
+    {
+        cerr << "Keins Speicher zu verfügung" << endl;
 
-            //return EXIT_FAILURE;
-        }
+        // return EXIT_FAILURE;
+    }
 
-        {
-            // Weise Leinwand
-            memset(buffer, ' ', TOTAL_BUFFER_SIZE);
-            *(buffer + TOTAL_BUFFER_SIZE - 1) = '\0';  
-        }
-        {
-            // Line 1
-            *(buffer + START_ADDRESS_LINE01) = '+'; 
-            memset(buffer + START_ADDRESS_LINE01 + 1, '-', SIGN_PER_LINE -2 -1);
-            *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE -2) = '+';
-            *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE -1) = '\n'; 
+    {
+        // Weise Leinwand
+        memset(buffer, ' ', TOTAL_BUFFER_SIZE);
+        *(buffer + TOTAL_BUFFER_SIZE - 1) = '\0';
+    }
+    {
+        // Line 1
+        *(buffer + START_ADDRESS_LINE01) = '+';
+        memset(buffer + START_ADDRESS_LINE01 + 1, '-', SIGN_PER_LINE - 2 - 1);
+        *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE - 2) = '+';
+        *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE - 1) = '\n';
+    }
 
-        }
+    {
+        // Line 2
+        *(buffer + START_ADDRESS_LINE02) = '|';
+        *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE - 2) = '|';
+        *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE - 1) = '\n';
+    }
 
-        {
-            // Line 2
-            *(buffer + START_ADDRESS_LINE02) = '|'; 
-            *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE -2) = '|';
-            *(buffer + START_ADDRESS_LINE01 + SIGN_PER_LINE -1) = '\n'; 
-        }
-
-        {
-            // line 3
-            memcpy(buffer + START_ADDRESS_LINE03, buffer + START_ADDRESS_LINE01, SIGN_PER_LINE - 1);
-        }
-        {
-            const int lengh = strlen(argv[1]);
-            const int reallengh 
-
-        }
-
-
+    {
+        // line 3
+        memcpy(buffer + START_ADDRESS_LINE03, buffer + START_ADDRESS_LINE01, SIGN_PER_LINE - 1);
+    }
+    {
+        const int length = strlen(argv[1]);
+        const int reallength = length > MAX_TEXT_LENGTH ? MAX_TEXT_LENGTH : length;
+        memcpy(buffer + START_OF_TEXT, argv[1], reallength);
+    }
+    cout << buffer << endl;
+    delete[] buffer, buffer = nullptr;
 }
